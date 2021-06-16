a3b1239e71f99754d2e5f3e382ea94992dc09a41
LeandroMelloo <leandropaulino@brq.com>
2021-06-15 21:54:43 -0300

    implementação da paginação

--- projeto_cliente/cliente/serializers.py
+++ projeto_cliente/cliente/serializers.py
@@ -1,3 +1,4 @@
+from cliente.validators import *
 from rest_framework import serializers
 from cliente.models import Cliente
 
@@ -6,3 +7,18 @@ class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__' # disponibilazando todos os dados do Banco de Dados para visualização
+    
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+           raise serializers.ValidationError({'cpf':'Número de CPF inválido'})
+
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome':'O formato NOME está incorreto, inclua apenas letras'})
+        
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg':'O formato RG está incorreto, deve conter 9 dígitos'})
+        
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':'O formato CELULAR está incorreto, deve seguir este modelo: 11 91234-1234'})
+        
+        return data
--- projeto_cliente/setup/settings.py
+++ projeto_cliente/setup/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'cliente'
+    'cliente',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# definindo o numero de registro, pela paginação
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
