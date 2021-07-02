ac85fa0b66edeef83c06a28e0d6cd2b2f610fd41
LeandroMelloo <leandromoreno1909@hotmail.com>
2021-07-02 15:15:24 -0300

    implementando as rotas do backend

--- projeto_cliente/cliente_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\leandropaulino\desktop\curso_alura_api_rest_django\projeto_cliente\cliente_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
